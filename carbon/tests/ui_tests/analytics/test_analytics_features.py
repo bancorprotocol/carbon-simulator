f52ca1a47c01fb873405fceefe180c2fd7c86e2d
mikewcasale <mike.w.casale@gmail.com>
2022-11-18 13:24:06 -0800

    Adding pytest testing framework
    * use `pytest carbon/tests` to run all tests
    * use `pytest carbon/tests/ui_tests` to run all UI tests
    * use `pytest carbon/tests/ui_tests/simulations/test_both_linked_and_single_positions.py` to run a specific test file
    * see pytest docs (https://docs.pytest.org/en/7.1.x/how-to/usage.html) for details on how to setup calls to test single functions
    
    Reverted the test case to adhere to p_start / p_end vs high / low conventions
    
    Added pytest testing framework
    
    * use `pytest carbon/tests` to run all tests
    * use `pytest carbon/tests/ui_tests` to run all UI tests
    * use `pytest carbon/tests/ui_tests/simulations/test_both_linked_and_single_positions.py` to run a specific test file
    * see pytest docs (https://docs.pytest.org/en/7.1.x/how-to/usage.html) for details on how to setup calls to test single functions

---
+++ carbon/tests/ui_tests/analytics/test_analytics_features.py
@@ -0,0 +1,108 @@
+import subprocess
+import sys
+
+import pytest
+
+from carbon import CarbonPair, CarbonSimulatorUI, analytics as al
+
+
+def test_orderbook_explain():
+    """
+    Derrived from `passed_tests` notebook demo-3-2
+    """
+    Sim = CarbonSimulatorUI(
+        pair=CarbonPair("ETH", "USDC"), verbose=False, raiseonerror=True
+    )
+    CA = al.Analytics(Sim, verbose=True)
+    maxx = 3000
+    orders = tuple(
+        [
+            al.orders_nt("ETH", 100, 2000, maxx),
+            al.orders_nt("ETH", 100, 2500, 2700),
+            al.orders_nt("USDC", 1000 * 100, 1500, 500),
+            al.orders_nt("USDC", 1100 * 100, 1200, 1000),
+        ]
+    )
+
+    for o in orders:
+        Sim.add_order(o.tkn, o.amt, o.p_start, o.p_end)
+
+    max_liquidity = Sim.liquidity()["ETHUSDC"]["ETH"]
+    src_amounts = al.linspace(max_liquidity, 20)
+    CA.simulate_trades(60, CA.ASK)
+    CA.simulate_trades(70, CA.ASK)
+    trg_amounts = al.vec([CA.simulate_trades(size, CA.ASK) for size in src_amounts])
+
+    OB = al.OrderBook(src_amounts, trg_amounts, "ETH", "USDC")
+    assert (
+            OB.explain()
+            == "Source token = ETH, target token = USDC.\nAMM sells ETH for USDC.\nPrices are quoted in USDC per ETH.\nOrder book amounts are quoted in ETH."
+    )
+
+
+def test_orderbook_plot_token_amount_chart_text():
+    """
+    Derrived from `passed_tests` notebook demo-3-2
+    """
+    Sim = CarbonSimulatorUI(
+        pair=CarbonPair("ETH", "USDC"), verbose=False, raiseonerror=True
+    )
+    CA = al.Analytics(Sim, verbose=True)
+    maxx = 3000
+    orders = tuple(
+        [
+            al.orders_nt("ETH", 100, 2000, maxx),
+            al.orders_nt("ETH", 100, 2500, 2700),
+            al.orders_nt("USDC", 1000 * 100, 1500, 500),
+            al.orders_nt("USDC", 1100 * 100, 1200, 1000),
+        ]
+    )
+
+    for o in orders:
+        Sim.add_order(o.tkn, o.amt, o.p_start, o.p_end)
+
+    max_liquidity = Sim.liquidity()["ETHUSDC"]["ETH"]
+    src_amounts = al.linspace(max_liquidity, 20)
+    CA.simulate_trades(60, CA.ASK)
+    CA.simulate_trades(70, CA.ASK)
+    trg_amounts = al.vec([CA.simulate_trades(size, CA.ASK) for size in src_amounts])
+
+    OB = al.OrderBook(src_amounts, trg_amounts, "ETH", "USDC")
+    result = OB.plot_token_amount_chart()
+    assert result == "plotted tokens received against trade size (504,750)"
+
+
+# The following tests the matplotlib functionality using the `mpl` fixture
+# https://github.com/matplotlib/pytest-mpl/blob/main/tests/test_pytest_mpl.py
+#
+# @pytest.mark.mpl_image_compare(baseline_dir='/carbon/tests/ui_tests/analytics/baseline',
+#                                filename='orderbook_plot_token_amount_chart_1.png',
+#                                tolerance=100)
+# def test_orderbook_plot_token_amount_chart():
+#     """
+#     Derrived from `passed_tests` notebook demo-3-2
+#     """
+#     Sim = CarbonSimulatorUI(pair=CarbonPair("ETH", "USDC"), verbose=False, raiseonerror=True)
+#     CA = al.Analytics(Sim, verbose=True)
+#     maxx = 3000
+#     orders = tuple([
+#         al.orders_nt("ETH", 100, 2000, maxx),
+#         al.orders_nt("ETH", 100, 2500, 2700),
+#         al.orders_nt("USDC", 1000 * 100, 1500, 500),
+#         al.orders_nt("USDC", 1100 * 100, 1200, 1000),
+#     ])
+#
+#     for o in orders:
+#         Sim.add_order(o.tkn, o.amt, o.p_start, o.p_end)
+#
+#     max_liquidity = Sim.liquidity()["ETHUSDC"]["ETH"]
+#     src_amounts = al.linspace(max_liquidity, 20)
+#     CA.simulate_trades(60, CA.ASK)
+#     CA.simulate_trades(70, CA.ASK)
+#     trg_amounts = al.vec([
+#         CA.simulate_trades(size, CA.ASK) for size in src_amounts
+#     ])
+#
+#     OB = al.OrderBook(src_amounts, trg_amounts, "ETH", "USDC")
+#
+#     return OB.plot_token_amount_chart(return_fig=True)
---
+++ carbon/tests/ui_tests/simulations/test_unrelated_positions.py
@@ -0,0 +1,19 @@
+from carbon import CarbonSimulatorUI
+
+
+def test_unrelated_token_in_add_order():
+    Sim = CarbonSimulatorUI(pair="ETHUSDC", verbose=False, raiseonerror=False)
+
+    response = Sim.add_order("LINK", 100, 2000, 2000)
+    assert response["success"] is False
+    assert (
+        response["error"]
+        == "('Invalid token specification (tkn not part of isopair)', 'ETHUSDC', 'LINK')"
+    )
+
+    response = Sim.add_order("DNE", 100, 2000, 2000)
+    assert response["success"] is False
+    assert (
+        response["error"]
+        == "('Invalid token specification (tkn not part of isopair)', 'ETHUSDC', 'DNE')"
+    )
