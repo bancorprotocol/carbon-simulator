0c60c942d01c2878c4cd3014fbfdd7b4d287d405
sklbancor <109073706+sklbancor@users.noreply.github.com>
2022-12-16 18:54:12 +0000

    Demo3-2: new order book methodology

--- resources/demo/CarbonSim-Demo-3-2.py
+++ resources/demo/CarbonSim-Demo-3-2.py
@@ -14,7 +14,7 @@
 #     name: python3
 # ---
 
-from carbon import CarbonPair, CarbonSimulatorUI, analytics as al
+from carbon import CarbonPair, CarbonOrderUI, CarbonSimulatorUI, analytics as al
 from carbon import __version__ as cversion, __date__ as cdate
 import numpy as np
 from matplotlib import pyplot as plt
@@ -27,21 +27,25 @@ print (f"Analytics Version v{al.__version__} ({al.__date__})")
 #
 # In this demo we look at an **order book**
 
-NUM_POINTS = 50   # number of points on the precise chart
+NUM_POINTS = 1000   # number of points on the precise chart
 
 # ## Setup
 
-Sim = CarbonSimulatorUI(pair="ETH/USDC", verbose=False, raiseonerror=True)
+ETHUSDC = CarbonPair(tknq="USDC", tknb="ETH")
+Sim = CarbonSimulatorUI(pair=ETHUSDC, verbose=False, raiseonerror=True)
 CA = al.Analytics(Sim, verbose=True)
 CA
 
-maxx = 3000
 orders = tuple([
-    al.orders_nt("ETH", 100, 2000, maxx),
-    al.orders_nt("ETH", 100, 2400, 2500),
-    al.orders_nt("ETH", 100, 2500, 2700),
+    al.orders_nt("ETH", 100, 2000, 3000),
+    al.orders_nt("ETH", 100, 2100, 2550),
+    al.orders_nt("ETH", 50, 2300, 2450),
+    al.orders_nt("ETH", 75, 2400, 2500),
+    al.orders_nt("ETH", 80, 2500, 2700),
     #al.orders_nt("ETH", 100, 2200, 2600),
     al.orders_nt("USDC", 1000*150, 1500, 500),
+    al.orders_nt("USDC", 1000*50, 1500, 1300),
+    al.orders_nt("USDC", 1000*20, 1450, 1350),
     al.orders_nt("USDC", 1100*150, 1200, 1000),
 ])
 
@@ -51,10 +55,13 @@ Sim.state()["orders"]
 
 Sim.liquidity(Sim.ASDF)
 
-# ## Approximate liquidity
+prices = al.linspace(400,3000, NUM_POINTS)
+prices
 
-prices = al.linspace(400,3000, 500)
-ETHUSDC = CarbonPair(tknq="USDC", tknb="ETH")
+curves_by_pair_bidask = CarbonOrderUI.curves_by_pair_bidask(Sim.state()["orderuis"])
+print(list(curves_by_pair_bidask.keys()))
+
+# ## Approximate liquidity
 
 liq =  al.calc_liquidity_approx(Sim.state()["orderuis"], prices, ETHUSDC, reverse=False)
 liqr = al.calc_liquidity_approx(Sim.state()["orderuis"], prices, ETHUSDC, reverse=True)
@@ -63,27 +70,42 @@ al.plot_approx_orderbook_chart(liq)
 
 al.plot_approx_orderbook_chart(liqr)
 
-# ## AMM sells base token (ask side)
+# ## AMM SELLS base token (ASK side)
 
-max_liquidity_eth = Sim.liquidity()["ETHUSDC"]["ETH"]
-print(f'sim liquidity {Sim.liquidity()["ETHUSDC"]["ETH"]} ETH')
-print("max liquidity", max_liquidity_eth)
-src_amounts_eth = al.linspace0(max_liquidity_eth*1.1, NUM_POINTS)
+# +
+# This code calculates the orderbook using the old method, with similates trades, ie 
+# using the standard routing algo; this is highly inefficient
 
-CA.simulate_trades(60, CA.ASK)
+# max_liquidity_eth = Sim.liquidity()["ETHUSDC"]["ETH"]
+# print(f'sim liquidity {Sim.liquidity()["ETHUSDC"]["ETH"]} ETH')
+# print("max liquidity", max_liquidity_eth)
+# src_amounts_eth = al.linspace0(max_liquidity_eth*1.1, NUM_POINTS)
 
-CA.simulate_trades(70, CA.ASK)
+# CA.simulate_trades(60, CA.ASK)
+# CA.simulate_trades(70, CA.ASK)
+# trg_amounts = al.vec([
+#     CA.simulate_trades(size, CA.ASK) for size in src_amounts_eth
+# ])
+# -
 
-trg_amounts = al.vec([
-    CA.simulate_trades(size, CA.ASK) for size in src_amounts_eth
-])
+curves = curves_by_pair_bidask["ETH/USDC"]["ASK"]
+c0 = curves[0]
+print(f"pair={c0.pair.slashpair} [{c0.pair.price_convention}] tkny={c0.tkny} tknx={c0.tknx}")
+dy_p = lambda p: sum(c.dyfromp_f(p) for c in curves)
+dx_p = lambda p: sum(c.dxfromdy_f(c.dyfromp_f(p)) for c in curves)
+dy_amounts = [dy_p(p) for p in prices]
+dx_amounts = [dx_p(p) for p in prices]
 
-OB = al.OrderBook(src_amounts_eth, trg_amounts, "ETH", "USDC")
+plt.plot(prices, dy_amounts)
+plt.xlabel(f"market price [{c0.pair.price_convention}]")
+plt.ylabel(f"Cumulative amount of y sold [{c0.tkny}]")
+
+plt.plot(prices, dx_amounts)
+plt.xlabel(f"market price [{c0.pair.price_convention}]")
+plt.ylabel(f"Cumulative amount of x bought [{c0.tknx}]")
+
+OB = al.OrderBook(dy_amounts, dx_amounts, "ETH", "USDC")
 print(OB.explain())
-
-OB
-
-trg_amounts
 
 OB.plot_token_amount_chart()
 
@@ -95,18 +117,27 @@ OB.plot_price_chart()
 
 OB.plot_orderbook_chart()
 
-# ## AMM sells base token (bid side)
+# ## AMM BUYS base token (BID side)
 
-max_liquidity_usdc = Sim.liquidity()["ETHUSDC"]["USDC"]
-print(f'sim liquidity {Sim.liquidity()["ETHUSDC"]["USDC"]} USDC')
-print("max liquidity", max_liquidity_usdc)
-src_amounts_eth2 = al.linspace0(max_liquidity_usdc/1000*1.1, NUM_POINTS)
+# +
+# max_liquidity_usdc = Sim.liquidity()["ETHUSDC"]["USDC"]
+# print(f'sim liquidity {Sim.liquidity()["ETHUSDC"]["USDC"]} USDC')
+# print("max liquidity", max_liquidity_usdc)
+# src_amounts_eth2 = al.linspace0(max_liquidity_usdc/1000*1.1, NUM_POINTS)
+# trg_amounts = al.vec([
+#     CA.simulate_trades(size, CA.BID) for size in src_amounts_eth2
+# ])
+# -
 
-trg_amounts = al.vec([
-    CA.simulate_trades(size, CA.BID) for size in src_amounts_eth2
-])
+curves = curves_by_pair_bidask["ETH/USDC"]["BID"]
+c0 = curves[0]
+print(f"pair={c0.pair.slashpair} [{c0.pair.price_convention}] tkny={c0.tkny} tknx={c0.tknx}")
+dy_p = lambda p: sum(c.dyfromp_f(p) for c in curves)
+dx_p = lambda p: sum(c.dxfromdy_f(c.dyfromp_f(p)) for c in curves)
+dy_amounts = [dy_p(p) for p in prices]
+dx_amounts = [dx_p(p) for p in prices]
 
-OB2 = al.OrderBook(src_amounts_eth2, trg_amounts, "ETH", "USDC", bidask=al.OrderBook.BID)
+OB2 = al.OrderBook(dx_amounts, dy_amounts, "ETH", "USDC", bidask=al.OrderBook.BID)
 print(OB2.explain())
 
 OB2.plot_token_amount_chart()
