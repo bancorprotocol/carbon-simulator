59903654f3b5a6b80ea37ceb7b8b2a428d4946b6
sklbancor <109073706+sklbancor@users.noreply.github.com>
2023-01-09 10:42:47 +0000

    Corrected tests

--- carbon/tests/ui_tests/simulations/test_alpha_router.py
+++ carbon/tests/ui_tests/simulations/test_alpha_router.py
@@ -14,10 +14,10 @@ def test_alpha_routing_trade_by_src():
     eth_trade_amount = 0.5
 
     AlphaSim.amm_buys('USDC', usdc_trade_amount, execute=False, threshold_orders=3)  # route_trade_by_source
-    assert AlphaSim.state()["trades"].iloc[3]["amt1"] == 0.499122
+    assert AlphaSim.state()["trades"].iloc[3]["amt1"] == 0.499121771875
 
     AlphaSim.amm_buys('ETH', eth_trade_amount, execute=False, threshold_orders=3)  # route_trade_by_source
-    assert AlphaSim.state()["trades"].query("uid=='1'").iloc[0]["amt1"] == 1388.325436
+    assert AlphaSim.state()["trades"].query("uid=='1'").iloc[0]["amt1"] == 1388.325436499391
 
 
 def test_alpha_routing_trade_by_target():
@@ -33,10 +33,10 @@ def test_alpha_routing_trade_by_target():
     eth_trade_amount = 0.5
 
     AlphaSim.amm_sells('USDC', usdc_trade_amount, execute=False, threshold_orders=3)  # route_trade_by_target
-    assert AlphaSim.state()["trades"].query("uid=='0'").iloc[0]["amt2"] == 0.359301
+    assert AlphaSim.state()["trades"].query("uid=='0'").iloc[0]["amt2"] == 0.35930099786
 
     AlphaSim.amm_sells('ETH', eth_trade_amount, execute=False, threshold_orders=2)  # route_trade_by_target
-    assert AlphaSim.state()["trades"].query("uid=='1'").iloc[0]["amt2"] == 1002.646305
+    assert AlphaSim.state()["trades"].query("uid=='1'").iloc[0]["amt2"] == 1002.64630467044
 
     AlphaSim.amm_sells('ETH', eth_trade_amount, execute=False, threshold_orders=4)  # route_trade_by_target
-    assert AlphaSim.state()["trades"].query("uid=='2'").iloc[0]["amt2"] == 1001.321404
+    assert AlphaSim.state()["trades"].query("uid=='2'").iloc[0]["amt2"] == 1001.321403916542
