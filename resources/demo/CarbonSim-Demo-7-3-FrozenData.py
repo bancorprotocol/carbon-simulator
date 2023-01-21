d84755bcd4d6d70f1cdbd2fb0a2ea39c51c1a045
sklbancor <109073706+sklbancor@users.noreply.github.com>
2023-01-21 15:22:49 +0000

    Demo 7-3: simplified, using helpers

--- resources/demo/CarbonSim-Demo-7-3-FrozenData.py
+++ resources/demo/CarbonSim-Demo-7-3-FrozenData.py
@@ -13,159 +13,60 @@
 #     name: python3
 # ---
 
-from carbon import CarbonSimulatorUI, CarbonOrderUI, P, __version__, __date__
-from math import sqrt, exp, log
-import numpy as np
-import pandas as pd
-from matplotlib import pyplot as plt
+# +
+from carbon.helpers.stdimports import *
+from carbon.helpers.pdread import *
+from carbon.helpers.strategy import *
+from carbon.helpers.fls import *
+
 plt.style.use('seaborn-dark')
 plt.rcParams['figure.figsize'] = [12,6]
-from dataclasses import dataclass
-from time import time as timestamp
-from os.path import join as j
-import os
-print(f"Carbon v{__version__} ({__date__})")
-print("{0.__name__} v{0.__VERSION__} ({0.__DATE__})".format(CarbonSimulatorUI))
-print("{0.__name__} v{0.__VERSION__} ({0.__DATE__})".format(CarbonOrderUI))
+# -
 
 # # Carbon Simulation - Demo 7-3 (frozen data)
 
 # ## Setup
 
-DATAPATH = "../data"
-DATAID = "RANPTH-05000-0000"
-DATAFN = j(DATAPATH, f"{DATAID}.pickle")
-DATAFN
+# ### Path data
+# filename determines collection, eg `RANPTH-05000-0000` is sig=50% vol and mu=0% drift; see available collections in the `ls` command below
 
 # +
-# #!ls {DATAPATH}
+DATAID = "RANPTH-05000-0000"
+
+DATAPATH = "../data"
+DATAFN = j(DATAPATH, f"{DATAID}.pickle")
+print(f"Chose data id {DATAID}")
 # -
 
-OUTPATH = "/Users/skl/Desktop/sim7-3"
-OUTPATH = None
-if OUTPATH:
+# !ls {DATAPATH}/*.pickle
+
+# ### Generated output
+#
+# If `OUTPATH` is `None`, output will not be saved, otherwise it will be saved to the indicated directory (use `"."` for current)
+
+OUTPATH = "/Users/skl/Desktop/sim7-3"       # uncomment to save charts in specific location
+OUTPATH = "."                               # uncomment to save charts in current directory
+OUTPATH = None                              # uncomment to not save charts
+if OUTPATH and OUTPATH != ".":
     # !mkdir {OUTPATH}
     # !rm {OUTPATH}/*.png
+print(f"OUTPATH = {OUTPATH}")
 
-
-# ## Code
-
-# ### Data retrieval
-
-# +
-def dfread(fn):
-    """reads dataframe from file and asserts format"""
-    
-    if fn[-7:] == ".pickle":
-        df = pd.read_pickle(fn)
-    else:
-        df = pd.read_csv(fn)
-    assert df.columns[0] == "time"
-    assert df.columns[1] == "datetime"
-    return df
-    
-def pdread(fn, datacol=None, indexcol=None):
-    """
-    reads a dataframe and returns a single column with index
-    
-    :fn:        the (full) filename
-    :datacol:   name or index of the data col; None returns frame
-    :indexcol:  name of the index col (default: "datetime")
-    :returns:   pandas series
-    """
-    if indexcol is None: indexcol = "datetime"
-
-    df = dfread(fn)
-    df = df.set_index(indexcol)
-    if datacol is None:
-        return df.iloc[:, 1:]
-    elif isinstance(datacol, str):
-        return df[datacol]
-    elif isinstance(datacol, int):
-        return df.iloc[:, datacol+1]
-    else:
-        raise ValueError("datacol must be None, str or int", datacol)
-
-def pdcols(fn):
-    """
-    reads a dataframe and returns a single column with index
-    
-    :fn:        the (full) filename
-    :returns:   the column names (excluding the first two)
-    """
-    return dfread(fn).columns[2:]
-
-def pathtime(path):
-    """returns the time (in years) covered by the series `path`"""
-    return (path.index[-1]-path.index[0])/pd.Timedelta(days=1)/365.25
-
-
-# -
-
-# ### Strategy encapsulation
-
-# +
-@dataclass
-class strategy():
-    
-    p_buy_a: float
-    p_buy_b: float
-    p_sell_a: float
-    p_sell_b: float 
-
-    amt_rsk: float = 0
-    amt_csh: float = 0
-        
-    rsk: str = "RSK"
-    csh: str = "CSH"
-
-    @classmethod
-    def from_mwh(cls, mid, gap, width, amt_rsk=0, amt_csh=0, rsk="RSK", csh="CSH"):
-        """create class from mid and widths"""
-        p_buy_a = mid/(1+gap)
-        p_buy_b = mid/(1+gap)/(1+width)
-        p_sell_a = mid*(1+gap)
-        p_sell_b = mid*(1+gap)*(1+width)
-        return cls(p_buy_a, p_buy_b, p_sell_a, p_sell_b, amt_rsk, amt_csh, rsk, csh)
-    
-    @property
-    def descr(s):
-        bid_s = f"BID {s.p_buy_b:.1f}-{s.p_buy_a:.1f} [{s.amt_csh} {s.csh}]"
-        ask_s = f"ASK {s.p_sell_a:.1f}-{s.p_sell_b:.1f} [{s.amt_rsk} {s.rsk}]"
-        return f"{bid_s} -- {ask_s}"
-    
-    @property
-    def p(s):
-        """returns a tuple suitable for `add_strategy (*params)`"""
-        return (s.rsk, s.amt_rsk, s.p_sell_a, s.p_sell_b, s.amt_csh, s.p_buy_a, s.p_buy_b)
-    
-    @property
-    def slashpair(self):
-        return f"{self.rsk}/{self.csh}"
-
-strategy.from_mwh(2000, 0.1, 0.05, amt_rsk=1, rsk="ETH", csh="USD").p
-# -
-
-# ## Set strategies
+# ### Strategies
+#
+# This is the list of strategies to be tested against the paths. The 
 
 strats = (
-    strategy.from_mwh(100, 0.1, 0.05, amt_rsk=1, amt_csh=0),
-    strategy.from_mwh(100, 0.2, 0.05, amt_rsk=1, amt_csh=0),
-    strategy.from_mwh(100, 0.1, 0.2, amt_rsk=1, amt_csh=0),
-    strategy.from_mwh(100, 0.2, 0.2, amt_rsk=1, amt_csh=0),
+    strategy.from_mwh(m=100, g=0.10, w=0.05, amt_rsk=1, amt_csh=0),
+    strategy.from_mwh(m=100, g=0.20, w=0.05, amt_rsk=1, amt_csh=0),
+    strategy.from_mwh(m=100, g=0.10, w=0.20, amt_rsk=1, amt_csh=0),
+    strategy.from_mwh(m=100, g=0.20, w=0.20, amt_rsk=1, amt_csh=0),
 )
+#strats
 
-
-# ## Simulation
+# ## Simulation code
 
 def run_sim(strat, path):
-    """
-    runs a simulation 
-    
-    :strat:    the `strategy` object
-    :path:     simulation path (pandas or numpy series)
-    :returns:  tuple(rskamt_r, cshamt_r, value_r)
-    """
     Sim = CarbonSimulatorUI(pair=strat.slashpair)
     Sim.add_strategy(*strat.p)
     #print(strat.p)
@@ -185,10 +86,6 @@ def run_sim(strat, path):
 
 
 def plot_sim(strat, rskamt_f, cshamt_r, value_r, dataid):
-    """
-    plots the chart
-    """
-    
     rskamt_f  = rskamt_r[-1]
     cshamt_f  = cshamt_r[-1]
 
@@ -207,8 +104,9 @@ def plot_sim(strat, rskamt_f, cshamt_r, value_r, dataid):
     plt.legend(plots, labels)
     #print(f"t={   0:3.1f}: {rskamt_r[ 0]:5.1f} ETH {cshamt_r[ 0]:8.1f} USDC (value = {value_r[ 0]:6.1f} USDC)")
     #print(f"t={time:3.1f}: {rskamt_r[-1]:5.1f} ETH {cshamt_r[-1]:8.1f} USDC (value = {value_r[-1]:6.1f} USDC)")
-    
 
+
+# ## Simulation run
 
 for colnm in ["p0000", "p0001", "p0002"]:
     for ix, strat in enumerate(strats):
@@ -218,20 +116,23 @@ for colnm in ["p0000", "p0001", "p0002"]:
 
         rskamt_r, cshamt_r, value_r = run_sim(strat, path)
         plot_sim(strat, rskamt_r, cshamt_r, value_r, f"{DATAID} [{colnm}]")
-        if OUTPATH:
+        
+        # save charts
+        if isinstance(OUTPATH, str):
             ffn = j(OUTPATH, f"{DATAID}-{colnm}-{ix}.png")
             print(f"Saving figure {ffn}")
             plt.savefig(ffn)
         plt.show()
-        
+
 
 if OUTPATH:
     # !ls {OUTPATH}/*.png
 
-if OUTPATH:
-    from fls import fsave
+if OUTPATH and OUTPATH != ".":
     filelist = os.listdir(OUTPATH)
     filelist = [fn for fn in filelist if fn[-4:]==".png"]
     markdown = "\n\n".join(f"![]({OUTPATH}/{fn})" for fn in filelist)
     fsave(markdown, "_sim-charts.md", OUTPATH)
     # !pandoc {OUTPATH}/_sim-charts.md -o {OUTPATH}/_sim-charts.docx
+
+
