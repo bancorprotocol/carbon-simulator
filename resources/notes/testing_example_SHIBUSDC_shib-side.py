f4dacb8359eeb99f3c7a233cb026a04fc53968e2
NIXBNT <88088888+NIXBNT@users.noreply.github.com>
2023-02-13 10:35:05 +1100

    fix SHIBUSDC

--- resources/notes/testing_example_SHIBUSDC_shib-side.py
+++ resources/notes/testing_example_SHIBUSDC_shib-side.py
@@ -7,20 +7,20 @@ BITS_SIGNIFICANT =  40
 BITS_EXPONENT    =   8
 ONE_EXPONENT     =  48
 
-order_inputs = {        # EXAMPLE SHIB/USDC - SHIB-side
+order_inputs = {       # EXAMPLE SHIB/USDC - SHIB-side
 "pa" : 1/0.00001453,      # dy/dx - SHIB per USDC
 "pb" : 1/0.00001555,      # dy/dx - SHIB per USDC
-"y" : 100000,            # USDC tokens raw
-"z" : 100000,           # USDC tokens raw
+"y" : 100000,          # SHIB tokens raw
+"z" : 100000,          # SHIB tokens raw
 "decx" : 6,            # USDC decimality
-"decy" : 18,             # SHIB decimality
+"decy" : 18,           # SHIB decimality
 }
 
-amount = 10000       # trade 10000 SHIB for USDC
-tradeByTarget = False
+# amount = 1       # trade 100000 SHIB for USDC
+# tradeByTarget = False
 
-# amount = 1         # trade 1 USDC for SHIB
-# tradeByTarget = True
+amount = 1         # trade 1 USDC for SHIB
+tradeByTarget = True
 
 storage = create_order(order_inputs, BITS_SIGNIFICANT, BITS_EXPONENT, ONE_EXPONENT)
 trade(amount, tradeByTarget, storage, order_inputs)
--- resources/notes/testing_example_SHIBUSDC_usdc-side.py
+++ resources/notes/testing_example_SHIBUSDC_usdc-side.py
@@ -10,8 +10,8 @@ ONE_EXPONENT     =  48
 order_inputs = {        # EXAMPLE SHIB/USDC - USDC-side
 "pa" : 0.00001555,      # dy/dx - USDC per SHIB
 "pb" : 0.00001453,      # dy/dx - USDC per SHIB
-"y" :100000,            # SHIB tokens raw
-"z" : 100000,           # SHIB tokens raw
+"y" : 100000,            # USDC tokens raw
+"z" : 100000,           # USDC tokens raw
 "decx" : 18,            # SHIB decimality
 "decy" : 6,             # USDC decimality
 }
@@ -19,7 +19,7 @@ order_inputs = {        # EXAMPLE SHIB/USDC - USDC-side
 # amount = 1              # trade 1 USDC for SHIB
 # tradeByTarget = False
 
-amount = 10000        # trade 10000 SHIB for USDC
+amount = 1        # trade 100000 SHIB for USDC
 tradeByTarget = True
 
 storage = create_order(order_inputs, BITS_SIGNIFICANT, BITS_EXPONENT, ONE_EXPONENT)
