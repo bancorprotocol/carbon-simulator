80f14e45ba121e9ae5ed8349aeec7a348efc7cfe
sklbancor <109073706+sklbancor@users.noreply.github.com>
2023-03-15 16:48:35 +0000

    stdimports: iseq

--- carbon/helpers/stdimports.py
+++ carbon/helpers/stdimports.py
@@ -22,3 +22,30 @@ from .. import CarbonSimulatorUI, CarbonOrderUI, analytics as cal, P, __version_
 from .version import require_version
 from . import print_version, plt_style
 
+def iseq(arg0, *args, eps=1e-6):
+    """checks whether all arguments are equal to arg0, within tolerance eps if numeric"""
+    if not args:
+        raise ValueError("Must provide at least one arg", args)
+    try:
+        arg0+1
+        isnumeric = True
+    except:
+        isnumeric = False
+    #if isinstance(arg0, int) or isinstance(arg0, float):
+    if isnumeric:
+        # numeric testing
+        if arg0 == 0:
+            for arg in args:
+                if abs(arg) > eps: 
+                    return False
+                return True
+        for arg in args:
+            if abs(arg/arg0-1) > eps:
+                return False
+            return True
+    else:
+        for arg in args:
+            if not arg == arg0:
+                return False
+        return True
+
