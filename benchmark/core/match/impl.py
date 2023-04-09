e275bf08793b9ccd3cda185b31e1c44c6f819074
barak manos <>
2023-04-09 13:32:08 +0300

    Split the matching method to support two types of matching - Fast (the current one) and Best (a new one)

--- benchmark/core/match/impl.py
+++ benchmark/core/match/impl.py
@@ -1,5 +1,8 @@
 from functools import cmp_to_key
-from core.trade.impl import tradeBySourceAmount, tradeByTargetAmount, encodeOrder
+from core.trade.impl import tradeBySourceAmount, tradeByTargetAmount, encodeOrder, decodeFloat
+
+Fast = 'Fast'
+Best = 'Best'
 
 class Rate:
     def __init__(self, input, output):
@@ -33,22 +36,47 @@ def rateByTargetAmount(targetAmount, order):
     output = tradeByTargetAmount(input, order)
     return Rate(input, output)
 
-def cmpMin(x, y):
-    lhs = x.rate.output * y.rate.input
-    rhs = y.rate.output * x.rate.input
+def sortByMinRate(q1, q2):
+    lhs = q1.rate.output * q2.rate.input
+    rhs = q2.rate.output * q1.rate.input
     lt = lhs < rhs
     gt = lhs > rhs
     eq = not lt and not gt
-    is_lt = lt or (eq and x.rate.output < y.rate.output)
-    is_gt = gt or (eq and x.rate.output > y.rate.output)
+    is_lt = lt or (eq and q1.rate.output < q2.rate.output)
+    is_gt = gt or (eq and q1.rate.output > q2.rate.output)
     return is_lt - is_gt
 
-def cmpMax(x, y):
-    return cmpMin(y, x)
+def sortByMaxRate(q1, q2):
+    return sortByMinRate(q2, q1)
 
-def sortedQuotes(amount, orders, func, cmp):
-    quotes = [Quote(id, func(amount, orders[id])) for id in orders]
-    return sorted([quote for quote in quotes], key=cmp_to_key(cmp))
+def getParams(order):
+    y, z, A, B = [order[k] for k in 'yzAB']
+    return [y, z, decodeFloat(A), decodeFloat(B)]
+
+def getLimit(order):
+    y, z, A, B = getParams(order)
+    return (y * A + z * B) // z if z > 0 else 0
+
+def equalizeByTargetAmount(order, limit):
+    y, z, A, B = getParams(order)
+    return (y * A + z * (B - limit)) // A if A > 0 else y
+
+def equalizeBySourceAmount(order, limit):
+    return tradeByTargetAmount(equalizeByTargetAmount(order, limit), order)
+
+'''
+ * Sort the orders from best rate to worst rate:
+ * - Compute the rate of an order:
+ *   - Let `x` denote the maximum tradable amount not larger than `n`
+ *   - Let `y` denote the output amount of trading `x`
+ *   - The rate is determined as `y / x`
+ * - Compute the rates of two orders:
+ *   - If the rates are different, then the one with a better value prevails
+ *   - If the rates are identical, then the one with a better value of `y` prevails
+'''
+def sortedQuotes(amount, orders, trade, sort):
+    quotes = [Quote(id, trade(amount, orders[id])) for id in orders]
+    return sorted([quote for quote in quotes], key=cmp_to_key(sort))
 
 '''
  * Compute a list of {order id, trade amount} tuples:
@@ -57,20 +85,11 @@ def sortedQuotes(amount, orders, func, cmp):
  *   - Let `m` denote the maximum tradable amount not larger than `n`
  *   - Add the id of the order along with `m` to the output matching
  *   - If `m < n` then subtract `m` from `n` and continue, otherwise break
- *
- * Sorting the orders from best rate to worst rate:
- * - Computing the rate of an order:
- *   - Let `x` denote the maximum tradable amount not larger than `n`
- *   - Let `y` denote the output amount of trading `x`
- *   - The rate is determined as `y / x`
- * - Comparing the rates of two orders:
- *   - If the rates are different, then the one with a better value prevails
- *   - If the rates are identical, then the one with a better value of `y` prevails
 '''
-def matchFunc(amount, orders, filter, func, cmp):
+def matchFast(amount, orders, quotes, filter, trade):
     actions = []
 
-    for quote in sortedQuotes(amount, orders, func, cmp):
+    for quote in quotes:
         if amount > quote.rate.input:
             if filter(quote.rate):
                 actions.append(Action(quote.id, quote.rate.input, quote.rate.output))
@@ -80,18 +99,78 @@ def matchFunc(amount, orders, filter, func, cmp):
                 actions.append(Action(quote.id, quote.rate.input, quote.rate.output))
             break
         else: # amount < quote.rate.input
-            quote.rate = Rate(amount, func(amount, orders[quote.id]).output)
+            quote.rate = Rate(amount, trade(amount, orders[quote.id]).output)
             if filter(quote.rate):
                 actions.append(Action(quote.id, quote.rate.input, quote.rate.output))
             break
 
     return actions
 
-def matchBySourceAmount(amount, orders, filter):
-    return matchFunc(amount, orders, filter, rateBySourceAmount, cmpMin)
+'''
+ * Compute a list of {order id, trade amount} tuples:
+ * - Iterate the orders from best rate to worst rate:
+ *   - Calculate a trade which brings orders `0` thru `n - 1` to the rate of order `n`
+ *   - If the result is larger than or equal to the requested trade amount, then stop
+ * - If the result is larger than the requested trade amount:
+ *   - Determine a rate `r` between the rate of order `n - 1` and the rate of order `n`
+ *   - Calculate a trade which brings orders `0` thru `n - 1` to the rate `r`
+ *   - If the result is equal to the requested trade amount, then stop
+'''
+def matchBest(amount, orders, quotes, filter, trade, equalize):
+    orders = [orders[quote.id] for quote in quotes] + [{k: 0 for k in 'yzAB'}]
 
-def matchByTargetAmount(amount, orders, filter):
-    return matchFunc(amount, orders, filter, rateByTargetAmount, cmpMax)
+    for n in range(1, len(orders)):
+        limit = getLimit(orders[n])
+        rates = [trade(equalize(order, limit), order) for order in orders[:n]]
+        total = sum(rate.input for rate in rates)
+        delta = total - amount
+        if delta >= 0:
+            break
 
-def execute(method, amount, orders, filter = lambda rate : rate.input > 0 and rate.output > 0):
-    return globals()[method](int(amount), {id: encodeOrder(orders[id]) for id in range(len(orders))}, filter)
+    if delta > 0:
+        lo = limit
+        hi = getLimit(orders[n - 1])
+        while lo + 1 < hi:
+            limit = (lo + hi) // 2
+            rates = [trade(equalize(order, limit), order) for order in orders[:n]]
+            total = sum(rate.input for rate in rates)
+            delta = total - amount
+            if delta > 0:
+                lo = limit
+            elif delta < 0:
+                hi = limit
+            else: # delta == 0
+                break
+
+    if delta > 0:
+        for i in reversed(range(len(rates))):
+            rate = trade(rates[i].input - delta, orders[i])
+            delta += rate.input - rates[i].input
+            rates[i] = rate
+            if delta <= 0:
+                break
+    elif delta < 0:
+        for i in range(len(rates)):
+            rate = trade(rates[i].input - delta, orders[i])
+            delta += rate.input - rates[i].input
+            if delta > 0:
+                break
+            rates[i] = rate
+
+    return [Action(quotes[i].id, rates[i].input, rates[i].output) for i in range(len(rates)) if filter(rates[i])]
+
+def matchBy(amount, orders, types, filter, trade, sort, equalize):
+    quotes = sortedQuotes(amount, orders, trade, sort)
+    return {
+        Fast: matchFast(amount, orders, quotes, filter, trade) if Fast in types else None,
+        Best: matchBest(amount, orders, quotes, filter, trade, equalize) if Best in types else None,
+    }
+
+def matchBySourceAmount(amount, orders, types, filter):
+    return matchBy(amount, orders, types, filter, rateBySourceAmount, sortByMinRate, equalizeBySourceAmount)
+
+def matchByTargetAmount(amount, orders, types, filter):
+    return matchBy(amount, orders, types, filter, rateByTargetAmount, sortByMaxRate, equalizeByTargetAmount)
+
+def execute(method, amount, orders, types = [Fast, Best], filter = lambda rate : rate.input > 0 and rate.output > 0):
+    return globals()[method](int(amount), {id: encodeOrder(orders[id]) for id in range(len(orders))}, types, filter)
--- benchmark/test_match.py
+++ benchmark/test_match.py
@@ -1,30 +1,59 @@
 from util import read, write
-from core.match import impl
+from core.match import impl as match_impl
+from core.trade import impl as trade_impl
 
 def run(fileName):
     tests = read(__file__, fileName)
 
     for test in tests:
-        actions = impl.execute(test['method'], test['amount'], test['orders'])
-
-        formatString = '- order {{:>{}}}: input = {{:>{}}}, output = {{:>{}}}'.format(
-            max([len(str(action.id)) for action in actions]),
-            max([len(str(action.input)) for action in actions]),
-            max([len(str(action.output)) for action in actions]),
-        )
+        actions = match_impl.execute(test['method'], test['amount'], test['orders'])
 
         print(fileName.split('/')[-1], test['method'], test['amount'] + ':')
-        for action in actions:
-            print(formatString.format(action.id, action.input, action.output))
 
-        test['actions'] = [
-            {
-                'id': str(action.id),
-                'input': str(action.input),
-                'output': str(action.output),
-            }
-            for action in actions
-        ]
+        for type in actions:
+            print('-', type + ':')
+
+            formatString = '  - order {{:>{}}}: input = {{:>{}}}, output = {{:>{}}}'.format(
+                max([len(str(action.id)) for action in actions[type]]),
+                max([len(str(action.input)) for action in actions[type]]),
+                max([len(str(action.output)) for action in actions[type]]),
+            )
+
+            for action in actions[type]:
+                print(formatString.format(action.id, action.input, action.output))
+
+        [method, attribute, compare_input, compare_output] = {
+            'matchBySourceAmount': ['tradeBySourceAmount', 'output', int.__le__, int.__ge__],
+            'matchByTargetAmount': ['tradeByTargetAmount', 'input' , int.__ge__, int.__le__],
+        }   [test['method']]
+
+        for type in actions:
+            assert sum(action.input for action in actions[type]) <= int(test['amount'])
+            for action in actions[type]:
+                assert getattr(action, attribute) <= int(test['orders'][action.id]['liquidity'])
+                assert action.output == trade_impl.execute(method, action.input, test['orders'][action.id])
+
+        assert compare_input(
+            sum(action.input for action in actions[match_impl.Best]),
+            sum(action.input for action in actions[match_impl.Fast])
+        )
+
+        assert compare_output(
+            sum(action.output for action in actions[match_impl.Best]),
+            sum(action.output for action in actions[match_impl.Fast])
+        )
+
+        test['actions'] = {
+            type: [
+                {
+                    'id': str(action.id),
+                    'input': str(action.input),
+                    'output': str(action.output),
+                }
+                for action in actions[type]
+            ]
+            for type in actions
+        }
 
     write(__file__, fileName, tests)
 
